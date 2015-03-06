3afd1efb344d4bc4eac553250210f0e10564201f
John Plaice <cartesianprogramming@gmail.com>
2015-03-06 05:41:00 -0500

    Correct copyright date. 	modified:   zebrackets.py

--- src/zebrackets.py
+++ src/zebrackets.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-# Copyright (c) John Plaice, 2005
+# Copyright (c) John Plaice, 2015
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
